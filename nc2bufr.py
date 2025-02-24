6ba0782face8d673c8a7ef0e25a30d68f9a2240c
Anne-Claire Billault-Roux <anne-claire.billault-roux@meteoswiss.ch>
2025-02-24 17:04:40 +0100

    Added NC-2-BUFR function which uses custom template (wind + temperature). Works for 1 full profile

--- nc2bufr.py
+++ nc2bufr.py
@@ -124,12 +124,7 @@ def bufr_encode_309024(ibufr, dst):
 
 def bufr_encode_forloop_309024(ibufr,dst):
     """ This generates a valid BUFR with template 309024 (wind profiler).
-     """
-    # create the bufr structure using the template code
-    ivalues=(309024)
-    ec.codes_set(ibufr, 'unexpandedDescriptors', ivalues)
 
-    """
     3 09 024 (wind profiler template):
     3 01 132,
     2 01 151, 2 02 130, 0 02 121, 2 02 000, 2 01 000,
@@ -138,7 +133,99 @@ def bufr_encode_forloop_309024(ibufr,dst):
     0 07 007,
     3 01 021,
     0 11 003, 0 11 004, 0 33 002, 0 11 006, 0 33 002, 0 10 071, 0 27 079
+
+     """
+    # create the bufr structure using the template code
+    ivalues=(309024)
+    ec.codes_set(ibufr, 'unexpandedDescriptors', ivalues)
+
+    # set data keys and values
+    #################
+    # common header sequence 3 01 132: 3 01 150, 3 01 001, 3 01 021, 0 07 030, 0 08 021, 3 01 011, 3 01 12, 0 02 006, 0 01 079, 0 01 085
+    #-----------------
+    # 3 01 150 (WIGOS identifiers): 0 01 150, 0 01 126, 0 01 127, 0 01 128
+    ec.codes_set(ibufr, 'wigosIdentifierSeries', 0) # 0 01 150 -> wigos identifier series: TO DO VALUE
+    ec.codes_set(ibufr, 'wigosIssuerOfIdentifier', 20000) # 0 01 126 -> WIGOS issue of identifier: TO DO VALUE
+    ec.codes_set(ibufr, 'wigosIssueNumber', 0) # 0 01 127 -> WIGOS issue number: TO DO VALUE
+    ec.codes_set(ibufr, 'wigosLocalIdentifierCharacter', '06610') # 0 01 128 -> WIGOS local identifier (character)
+    #-----------------
+    # 3 01 001 (WMO block and station numbers): 0 01 001, 0 01 002
+    ec.codes_set(ibufr, 'blockNumber', 6) # 0 01 001 -> WMO block number TO DO VALUE
+    ec.codes_set(ibufr, 'stationNumber', 610) # 0 01 002 -> WMO station number TO DO VALUE
+    #-----------------
+    # 3 01 021 (Radiosonde launch point location): 0 08 041, 3 01 122, 3 01 021, 0 07 031, 0 07 007
+    # ec.codes_set(ibufr, 'dataSignificance', 1)  # 0 08 041 -> data significance, see p. 827 of WMO code book (1 = Observation site) # crashes (KeyValue not found)
+    # 3 01 122: Date/time (to hundredth of second): 3 01 011, 3 01 012, 2 01 135, 2 02 130, 0 04 006, 2 02 000, 2 01 000
+    # 3 01 121: Latitude / longitude (high accuracy): 0 05 001, 0 06 001
+    ec.codes_set(ibufr, '#1#latitude', float(dst.station_latitude)) # 0 05 001 -> latitude (high accuracy)
+    ec.codes_set(ibufr, '#1#longitude', float(dst.station_longitude)) # 0 06 001 -> longitude (high accuracy)
+    # 0 07 031: height of barometer above mean sea level TO DO NEEDED?
+    ec.codes_set(ibufr, '#1#height', 100) # 0 07 007 Height TO DO NEEDED?
+    #-------------------
+    ec.codes_set(ibufr, 'heightOfStationGroundAboveMeanSeaLevel', 200) # 0 07 030: height of station ground above mean sea level
+    ec.codes_set(ibufr, '#1#timeSignificance', 25) # 0 08 021 -> time significance, see p. 820 of wmo code book
+    #-------------------
+    # 3 01 011: Year, month, day
+    ec.codes_set(ibufr, 'year', datetime.datetime.fromtimestamp(dst.time.item()*1e-9).year) # 0 04 001: Year
+    ec.codes_set(ibufr, 'month', datetime.datetime.fromtimestamp(dst.time.item()*1e-9).month) # 0 04 002: Month
+    ec.codes_set(ibufr, 'day', datetime.datetime.fromtimestamp(dst.time.item()*1e-9).day) # 0 04 003: Day
+    #-------------------
+    # 3 01 012: Hour, minute
+    ec.codes_set(ibufr, 'hour', datetime.datetime.fromtimestamp(dst.time.item()*1e-9).hour) # 0 04 004: Hour
+    ec.codes_set(ibufr, 'minute', datetime.datetime.fromtimestamp(dst.time.item()*1e-9).minute) # 0 04 005: Minute
+    #-------------------
+    ec.codes_set(ibufr, 'upperAirRemoteSensingInstrumentType', 6) # 0 02 006: Upper air remote sensing instrument type -> TO DO check table p. 762, 6 is wind profiler, 7 is lidar
+    ec.codes_set(ibufr, 'uniqueIdentifierForProfile', 'EULIAAKL')    # 0 01 079: Unique identifier for the profile -> TO DO CHECK VALUE
+    ec.codes_set(ibufr, 'observingPlatformManufacturerModel','EULIAA' ) # 0 01 085: Observing platform manufacturer's model
+    ############################################
+
+
+    ec.codes_set(ibufr, '#2#timeSignificance', 2) # 0 08 021 -> time significance, see p. 820 of wmo code book (2: Time averaged)
+    ec.codes_set(ibufr, 'timePeriod', 1) # 0 04 025 -> Time period indicates the duration in minutes over which the measurements have been averaged
+
+    # 1 09 000 -> Delayed replication of 9 descriptors (defined before)
+    # ec.codes_set(ibufr, 'extendedDelayedReplicationFactor', ivalues) # 0 31 002: Extended delayed description replication factor? -> NO apparently this is read only
+
+    altitudes = dst.altitude_mie.values
+    # print(altitudes)
+    for i in range(len(altitudes)):
+        # print(i)
+        ec.codes_set(ibufr,'#%d#height'%(i+1), altitudes[i]) # 0 07 007 Height
+        ec.codes_set(ibufr,'#%d#latitude'%(i+2), float(dst.station_latitude)) # 0 05 001 -> latitude (high accuracy)
+        ec.codes_set(ibufr,'#%d#longitude'%(i+2), float(dst.station_longitude)) # 0 06 001 -> longitude (high accuracy)
+        ec.codes_set(ibufr,'#%d#u'%(i+1), float(dst.u_mie[i].values)) # 0 11 003 -> u-component
+        ec.codes_set(ibufr,'#%d#v'%(i+1), float(dst.v_mie[i].values)) # 0 11 004 -> v-component
+        ec.codes_set(ibufr,'#%d#qualityInformation'%(2*i+1), 0)  # 0 33 002 -> Quality information (0: Data not suspect, 1: Data suspect, 2: Reserved, 3: Quality information not given) TO DO adjust with flag in NC
+        ec.codes_set(ibufr,'#%d#w'%(i+1), float(dst.w_mie[i].values)) # 0 11 006 -> w-component
+        ec.codes_set(ibufr,'#%d#qualityInformation'%(2*i+2), 1) # 0 33 002 -> Quality information
+        ec.codes_set(ibufr,'#%d#verticalResolution'%(i+1), dst.vertical_resolution.item()) # 0 10 071 -> vertical resolution
+        ec.codes_set(ibufr,'#%d#horizontalWidthOfSampledVolume'%(i+1), 1.)  # 027079 -> horizontal width of sampled volume (m) TO DO CHECK VALUE
+
+    ec.codes_set(ibufr, 'pack', 1)  # Required to encode the keys back in the data section
+
+
+
+def bufr_encode_forloop(ibufr,dst):
     """
+    This generates a BUFR file using the following sequence.
+    Basically like wind profiler + temperature and associated quality descr.
+
+    301132, # common header sequence
+    201138, 202126, 2121, 202000, 201000, # mean freq
+    8021, 4025, # time significance and time period
+    111000, 31002, # delayed replication (11 parameters)
+    7007, 301021, # height and lat/lon
+    11003, 11004, 33002, 11006, 33002, 12001, 33002, # atm vars
+    10071, 27079, # vert res and horiz width
+    """
+
+    # create the bufr structure using the template code
+    ivalues=(301132, 201138, 202126, 2121, 202000, 201000, 8021, 4025, 111000, 31002,
+         7007, 301021,
+         11003, 11004, 33002, 11006, 33002, 12001, 33002,
+         10071, 27079,)
+    ec.codes_set_array(ibufr, 'unexpandedDescriptors', ivalues)
+
     # set data keys and values
     #################
     # common header sequence 3 01 132: 3 01 150, 3 01 001, 3 01 021, 0 07 030, 0 08 021, 3 01 011, 3 01 12, 0 02 006, 0 01 079, 0 01 085
@@ -195,9 +282,11 @@ def bufr_encode_forloop_309024(ibufr,dst):
         ec.codes_set(ibufr,'#%d#longitude'%(i+2), float(dst.station_longitude)) # 0 06 001 -> longitude (high accuracy)
         ec.codes_set(ibufr,'#%d#u'%(i+1), float(dst.u_mie[i].values)) # 0 11 003 -> u-component
         ec.codes_set(ibufr,'#%d#v'%(i+1), float(dst.v_mie[i].values)) # 0 11 004 -> v-component
-        ec.codes_set(ibufr,'#%d#qualityInformation'%(2*i+1), 0)  # 0 33 002 -> Quality information (0: Data not suspect, 1: Data suspect, 2: Reserved, 3: Quality information not given) TO DO adjust with flag in NC
+        ec.codes_set(ibufr,'#%d#qualityInformation'%(3*i+1), 0)  # 0 33 002 -> Quality information (0: Data not suspect, 1: Data suspect, 2: Reserved, 3: Quality information not given) TO DO adjust with flag in NC
         ec.codes_set(ibufr,'#%d#w'%(i+1), float(dst.w_mie[i].values)) # 0 11 006 -> w-component
-        ec.codes_set(ibufr,'#%d#qualityInformation'%(2*i+2), 1) # 0 33 002 -> Quality information
+        ec.codes_set(ibufr,'#%d#qualityInformation'%(3*i+2), 1) # 0 33 002 -> Quality information TO DO based on flag
+        ec.codes_set(ibufr,'#%d#airTemperature'%(i+1), float(dst.temperature_int[i])) # 0 12 001 -> Temperature / air temperature
+        ec.codes_set(ibufr,'#%d#qualityInformation'%(3*i+3), 2) # 0 33 002 -> Quality information TO DO based on flag
         ec.codes_set(ibufr,'#%d#verticalResolution'%(i+1), dst.vertical_resolution.item()) # 0 10 071 -> vertical resolution
         ec.codes_set(ibufr,'#%d#horizontalWidthOfSampledVolume'%(i+1), 1.)  # 027079 -> horizontal width of sampled volume (m) TO DO CHECK VALUE
 
@@ -207,21 +296,25 @@ def bufr_encode_forloop_309024(ibufr,dst):
 if __name__=='__main__':
 
     inputFilename = 'data/TestNC.nc'
-    outFilename = str(inputFilename.split('.')[0]+'_forloop.bufr')
+    outFilename = 'data/TestNC2BUFR_forloop_notemplate.bufr'
 
     ds_full = xr.open_dataset(inputFilename)
     ds = ds_full.isel(time=0)
-    # Subselection with valid data (following BUFR min/max) - TO DO implement in prior QC
+    # Subselection with valid data (following BUFR min/max) - TO DO implement in prior QC / LV1 stripped file
     ds = ds.sel(altitude_mie=slice(0,60e3))
-    ds.w_mie[ds.w_mie < -40.96]=np.nan
-    ds.w_mie[ds.w_mie > 40.96]=np.nan
-    ds.v_mie[ds.v_mie < -409.6] = np.nan
-    ds.v_mie[ds.v_mie > 409.6] = np.nan
+    ds = ds.sel(field_of_view='zenith')
+    ds.w_mie[(ds.w_mie < -40.96)| (ds.w_mie > 40.96)]=np.nan
+    ds.v_mie[(ds.v_mie < -409.6)|(ds.v_mie > 409.6)] = np.nan
+    ds.v_mie[(ds.u_mie < -409.6)|(ds.u_mie > 409.6)] = np.nan
+    ds.temperature_int[(ds.temperature_int > 409.5) | (ds.temperature_int < 0)]=np.nan
 
     # convert to BUFR
     fout = open(outFilename, "wb")
     bid = ec.codes_bufr_new_from_samples('BUFR4')
     bufr_encode_header(bid,ds)
-    bufr_encode_forloop_309024(bid,ds)
-    # bufr_encode_309024(bid, ds2)
+
+    # bufr_encode_forloop_309024(bid,ds)
+    # bufr_encode_309024(bid, ds)
+    bufr_encode_forloop(bid,ds)
+
     ec.codes_write(bid,fout)
