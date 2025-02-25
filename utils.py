a3fbe772fcf0ea7acf407cee986ab85b2d1bb177
Anne-Claire Billault-Roux <anne-claire.billault-roux@meteoswiss.ch>
2025-02-25 17:29:34 +0100

    Script to generate short NC with a single profile

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import yaml
 import numpy as np
+import xarray as xr
+
 
 def get_conf(file):
     """get config dictionary from yaml files"""
@@ -37,3 +39,12 @@ def compute_lat_lon(lat_station = 0, lon_station = 0, altitude = np.zeros(1), th
                                                         ), axis = -1))
 
     return (latitude_arr_3fov, longitude_arr_3fov)
+
+
+def flag_var(dsz,var_key, var_err_key, var_min_thres = -999., var_max_thres = 999., var_err_thres = 999.):
+    da_flag = xr.zeros_like(dsz[var_key])
+    data_suspect = dsz[var_err_key] > var_err_thres
+    data_invalid = (dsz[var_key]<var_min_thres ) | (dsz[var_key]>var_max_thres) | (xr.ufuncs.isnan(dsz[var_key]))
+    da_flag = da_flag.where(~data_suspect,1)
+    da_flag = da_flag.where(~data_invalid,2)
+    return da_flag
