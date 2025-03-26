25957d729b39d0598a51c62d7b47cc6a5d78fb0d
Anne-Claire Billault-Roux <anne-claire.billault-roux@meteoswiss.ch>
2025-03-26 17:16:27 +0100

    Did some significant refactoring

---
+++ measurement.py
@@ -0,0 +1,139 @@
+import xarray as xr
+from netCDF4 import Dataset
+import pandas as pd
+import yaml
+import numpy as np
+from utils import get_conf, correct_dim_scalar_fields, check_var_in_ds, compute_lat_lon, flag_var
+
+class Measurement():
+    def __init__(self, conf_nc_file, data=None, conf_qc_file=None):
+        self.conf_nc = get_conf(conf_nc_file)
+        if data:
+            self.data = data
+        else:
+            self.data = xr.Dataset()
+        if conf_qc_file:
+            self.qc_conf = get_conf(conf_qc_file)
+        self.config_dims = self.conf_nc['dimensions']['unlimited'] + self.conf_nc['dimensions']['fixed']
+        correct_dim_scalar_fields(self.conf_nc['variables'])
+
+
+    def add_var(self, var_dict):
+        """if additional var should be added separately
+        Input:
+            var_dict = {'var_name': (dim, data)} or {'var_name':(dim, data, {attrs})}
+        """
+        for var_name, var_data in var_dict.items():
+            self.data[var_name] = var_data
+
+    def add_lat_lon(self):
+        self.data['latitude_mie'], self.data['longitude_mie'] = compute_lat_lon(lat_station=self.data.station_latitude, lon_station=self.data.station_longitude, altitude=self.data.altitude_mie)
+        self.data['latitude_ray'], self.data['longitude_ray'] = compute_lat_lon(lat_station=self.data.station_latitude, lon_station=self.data.station_longitude, altitude=self.data.altitude_ray)
+
+
+    def add_quality_flag(self):
+        self.data['w_mie_flag'] = flag_var(self.data, 'w_mie', 'w_mie_err', var_min_thres=-self.qc_conf['W_ABS_THRES'], var_max_thres=self.qc_conf['W_ABS_THRES'], var_err_thres=self.qc_conf['W_ERR_THRES'])
+        self.data['u_mie_flag'] = flag_var(self.data, 'u_mie', 'u_mie_err', var_min_thres=-self.qc_conf['U_V_ABS_THRES'], var_max_thres=self.qc_conf['U_V_ABS_THRES'], var_err_thres=self.qc_conf['U_V_ERR_THRES'])
+        self.data['v_mie_flag'] = flag_var(self.data, 'v_mie', 'v_mie_err', var_min_thres=-self.qc_conf['U_V_ABS_THRES'], var_max_thres=self.qc_conf['U_V_ABS_THRES'], var_err_thres=self.qc_conf['U_V_ERR_THRES'])
+        self.data['temperature_int_flag'] = flag_var(self.data, 'temperature_int', 'temperature_int_err', var_min_thres=self.qc_conf['TEMP_MIN_THRES'], var_max_thres=self.qc_conf['TEMP_MAX_THRES'], var_err_thres=self.qc_conf['TEMP_ERR_THRES'])
+        self.data['u_v_flag']= xr.ufuncs.maximum(self.data.u_mie_flag,self.data.v_mie_flag)
+
+        if self.qc_conf['INVALID_TO_NAN']:
+            for var in ['u_mie', 'v_mie', 'w_mie', 'temperature_int']:
+                self.data[var] = self.data[var].where(self.data[var+'_flag']<1, np.nan)
+
+
+    def subsel_stripped_profile(self):
+        self.data = self.data.sel(altitude_mie=slice(0,self.qc_conf['MAX_ALTITUDE']),field_of_view='zenith')
+        self.data = self.data.isel(time=0)
+        self.data.w_mie.values = self.data.w_mie.values-self.qc_conf['CORRECTION'] # first file from iap has a velocity bias
+        self.data = self.data[self.qc_conf['VARS_TO_KEEP']]
+
+
+
+class H5Reader(Measurement):
+
+    def __init__(self, conf_nc_file, h5_data_file,**kwargs):
+        super().__init__(conf_nc_file,**kwargs)
+        self.data_file = h5_data_file
+
+
+    def read_hdf5_file(self, load_units=False):
+        """load hdf5 produced by IAP routine
+        Inputs:
+            hdf5_file: path to hdf5 file
+            load_units: whether or not to load the units datasets (in principle not used, units stored in config file)
+        Outputs:
+            rec: xarray Dataset with data from 'rec' group of hdf5 file; contains measurement data
+            glo: xarray Dataset with data from 'glo' group of hdf5 file; contains mostly metadata
+        """
+        nc = Dataset(self.data_file, diskless=True, persist=False)
+        nc2 = Dataset('/home/bia/Data/IAP/BankExport2.h5', diskless=True, persist=False) # For now I had to hardcode this because of an error in the first file - to be removed
+        self.rec = xr.open_dataset(xr.backends.NetCDF4DataStore(nc.groups.get('rec')))
+        self.glo = xr.open_dataset(xr.backends.NetCDF4DataStore(nc2.groups.get('glo')))
+        if load_units:
+            self.units_glo = xr.open_dataset(xr.backends.NetCDF4DataStore(nc.groups.get('units').groups.get('glo')))
+            self.units_rec = xr.open_dataset(xr.backends.NetCDF4DataStore(nc.groups.get('units').groups.get('rec')))
+
+
+    def load_attrs(self):
+        """prepare list of attributes; checks whether value should be fetched in hdf5"""
+        ds_attrs = self.conf_nc['attributes'].copy()
+        for attr in ds_attrs:
+            if self.conf_nc['attributes'][attr] is None:
+                ds_attrs[attr] = ''
+            if type(self.conf_nc['attributes'][attr])==dict:
+                if 'original_hdf5' in self.conf_nc['attributes'][attr].keys():
+                    hdf5_group = self.conf_nc['attributes'][attr]['original_hdf5']['hdf5_group']
+                    if hdf5_group == 'rec':
+                        hdf5_ds = self.rec
+                    elif hdf5_group == 'glo':
+                        hdf5_ds = self.glo
+                    ds_attrs[attr] = hdf5_ds[self.conf_nc['attributes'][attr]['original_hdf5']['hdf5_var_name']].data.item()
+        self.data.attrs = ds_attrs
+
+
+    def load_data(self):
+        """load the data from the hdf5 file or config"""
+
+        for var, specs in self.conf_nc['variables'].items():
+            if var in ['latitude_mie', 'latitude_ray', 'longitude_mie', 'longitude_ray']:
+                print('lat/lon computed at the end')
+                continue
+            # Load value from config if exists
+            if 'value' in specs and not(specs['value'] is None):
+                self.data[var] = (specs['dim'], specs['value'])
+
+            # Find hdf5 group
+            if (not ('original_hdf5' in specs.keys())) or (not ('hdf5_group' in specs['original_hdf5'].keys()))\
+                  or (not (specs['original_hdf5']['hdf5_group'] in ['rec', 'glo'])) or (not ('hdf5_var_name' in specs['original_hdf5'].keys())) :
+                print(var, ': this variable was not implemented in the hdf5 or the hdf5_group is invalid')
+                continue
+            hdf5_ds = self.rec if specs['original_hdf5']['hdf5_group'] == 'rec' else self.glo
+
+            hdf5_var = specs['original_hdf5']['hdf5_var_name']
+            if not check_var_in_ds(hdf5_ds, hdf5_var):
+                print(var,': No corresponding variable in original hdf5 file')
+                continue
+            if type(hdf5_var)==list:
+                self.data[var] = (specs['dim'], np.stack([hdf5_ds[var_fov].data for var_fov in hdf5_var],axis=-1))
+            else:
+                self.data[var] = (specs['dim'],  hdf5_ds[hdf5_var].data)
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+
+    hdf5file = '/home/bia/Data/IAP/BankExport.h5'
+    config = '/home/bia/euliaa_postproc/config_nc.yaml'
+    # output_nc = 'TestNC_var_per_fov2.nc'
+    output_nc = '/home/bia/euliaa_postproc/data/TestNC2.nc'
+    meas = Measurement_L2A(config,hdf5file)
+    meas.read_hdf5_file()
+    meas.load_attrs()
+    meas.load_data()
---
+++ write_netcdf.py
@@ -0,0 +1,71 @@
+import xarray as xr
+from netCDF4 import Dataset
+import pandas as pd
+import yaml
+import numpy as np
+from utils import get_conf, correct_dim_scalar_fields, compute_lat_lon
+ENC_NO_FILLVALUE = None
+
+class Writer():
+
+    def __init__(self, data_in, output_file, conf_nc_file):
+        self.output_file = output_file
+        self.conf_nc = get_conf(conf_nc_file)
+        self.data = data_in
+        self.config_dims = self.conf_nc['dimensions']['unlimited'] + self.conf_nc['dimensions']['fixed']
+        correct_dim_scalar_fields(self.conf_nc['variables'])
+
+    def set_fillvalue(self, var, specs):
+        """set the fill value of var by taking care not to remove any fill value for dimensions for CF compliance
+
+        Args:
+            var (str): the name of the variable of whom the dimension shall be checked
+            specs: specifications for this variable from config. Must contain the key 'dim' with a list of dimensions.
+        """
+        if var in self.config_dims or specs['_FillValue'] is None:  # using None in fillna (else clause) destroys dtype
+            self.data[var].encoding.update(_FillValue=ENC_NO_FILLVALUE)
+        else:
+            self.data[var] = self.data[var].fillna(specs['_FillValue'])  # don't use with _FillValue=None, dtype problem
+            self.data[var].encoding.update(_FillValue=specs['_FillValue'])
+
+    def write_nc(self):
+        """write netCDF file - copied from mwr_raw2l1, for clean nc writing"""
+        self.data.encoding.update(
+            unlimited_dims=self.conf_nc['dimensions']['unlimited']
+        )
+        for var in list(self.data.data_vars)+list(self.data.coords):
+            if not(var in self.conf_nc['variables'].keys()):
+                print('Warning, %s not in config keys'%var)
+                continue
+            specs = self.conf_nc['variables'][var]
+            self.set_fillvalue(var,specs)
+            if 'type' in specs.keys():
+                self.data[var].encoding.update(dtype=specs['type']) # TO DO does this really work
+            if 'attributes' in specs.keys():
+                self.data[var].attrs.update(specs['attributes'])
+        self.data.to_netcdf(self.output_file)
+
+
+
+if __name__=='__main__':
+
+    hdf5file = '/home/bia/Data/IAP/BankExport.h5'
+    config = '/home/bia/euliaa_postproc/configs/config_nc.yaml'
+    config_qc = '/home/bia/euliaa_postproc/configs/config_qc.yaml'
+    # output_nc = 'TestNC_var_per_fov2.nc'
+    output_nc_l2A = '/home/bia/euliaa_postproc/data/TestNC_L2A.nc'
+    output_nc_l2B = '/home/bia/euliaa_postproc/data/TestNC_L2B.nc'
+
+    from measurement import Measurement, H5Reader
+    meas = H5Reader(config, hdf5file,conf_qc_file=config_qc)
+    meas.read_hdf5_file()
+    meas.load_attrs()
+    meas.load_data()
+    meas.add_lat_lon()
+    meas.add_quality_flag()
+    nc_writer = Writer(meas.data,output_file=output_nc_l2A,conf_nc_file=config)
+    nc_writer.write_nc()
+    print('Wrote L2A successfully\n')
+    meas.subsel_stripped_profile()
+    nc_writer_l2b = Writer(meas.data,output_file=output_nc_l2B,conf_nc_file=config)
+    nc_writer_l2b.write_nc()
